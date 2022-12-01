fc7845a5bba19d48634603cf540df4900fabc20e
Niilo Lehner <niilo.lehner@gmail.com>
2022-12-01 16:01:59 +0200

    Lay out UI_Manager

--- Drive_Nice_Or_Else/Assets/Scripts/UI_Manager.cs
+++ Drive_Nice_Or_Else/Assets/Scripts/UI_Manager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 // WORK IN PROGRESS BY NIILO
 
 public class UI_Manager : MonoBehaviour
 {
+    // Declare variables
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +20,45 @@ public class UI_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void StopGo_OnClick()
+    {
+
+    }
+
+    private void ChangeLane_OnClick()
+    {
+
+    }
+
+    public void UpdateLife()
+    {
+
+    }
+
+    public void UpdateTimer()
+    {
+
+    }
+
+    public void ShowGameOver()
+    {
+
+    }
+
+    public void ShowQuestion()
+    {
+
+    }
+
+    private void YesAnswer_OnClick()
+    {
+
+    }
+
+    private void NoAnswer_OnClick()
+    {
+
     }
 }
