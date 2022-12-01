55d512bb4067150ecaf21e666e4387e436ad6c87
Niilo Lehner <niilo.lehner@gmail.com>
2022-12-01 20:24:13 +0200

    Game_Manager functional (still needs function calls to other classes)

--- Drive_Nice_Or_Else/Assets/Scripts/Game_Manager.cs
+++ Drive_Nice_Or_Else/Assets/Scripts/Game_Manager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 // WORK IN PROGRESS BY NIILO
@@ -7,33 +9,87 @@ using UnityEngine;
 public class Game_Manager : MonoBehaviour
 {
     // Declare variables/objects
+    UI_Manager ui_manager;
+
+    private int life = 3; // DUMMY life variable, ___REMOVE___ as soon there is the real deal from Life Manager
+
+    public bool isQuestionCorrect; // is the question correct?
+    public bool isGameOver; // is the game over?
 
 
     // Start is called before the first frame update
     void Start()
     {
         // initialize variables/objects
+        ui_manager = FindObjectOfType<UI_Manager>();
 
+        isGameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // check if life is at zero or below, if so, sets game over state
+        if(life <= 0) // function call to Life Manager (get life)
+        {
+            SetGameOver();
+        }
     }
 
+    // starts a new game from scratch (reload the scene)
     public void StartNewGame()
     {
-
+        SceneManager.LoadScene(0);
     }
 
+    // set the game over state
     public void SetGameOver()
     {
+        isGameOver = true;
 
+        // show the gameOverPanel
+        ui_manager.ShowGameOverPanel("DUMMY BestScore"); // function call to Score Manager (load BestScore)
+    }
+
+    // quit the game, depending if in editor or live app, change method
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
     }
 
     public void SetQuestionPhase()
     {
+        // function call to Question Manager (get random question and if answer is correct or wrong)
 
+        ui_manager.ShowQuestionPanel("Is this a DUMMY question text?");
+    }
+
+    // the answer given was yes
+    public void YesAnswer()
+    {
+        if (isQuestionCorrect == true) // player has answered correctly
+        {
+            // add function call to Life Manager (add life)
+        }
+        else // player has answered incorrectly
+        {
+            // add function call to Life Manager (minus life)
+        }
+    }
+
+    // the answer given was no
+    public void NoAnswer()
+    {
+        if (isQuestionCorrect == false) // player has answered correctly
+        {
+            // add function call to Life Manager (add life)
+        }
+        else // player has answered incorrectly
+        {
+            // add function call to Life Manager (minus life)
+        }
     }
 }
--- Drive_Nice_Or_Else/Assets/Scripts/UI_Manager.cs
+++ Drive_Nice_Or_Else/Assets/Scripts/UI_Manager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 using System;
@@ -47,16 +46,19 @@ public class UI_Manager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI highscore;
 
-    private bool isStopped; // car is stopped
-    private bool isRight; // car is on right lane
-    private bool isCorrect; // answer is correct
+   Game_Manager game_Manager;
+
+    private bool isStopped; // is car stopped?
+    private bool isRight; // is car on right lane?
 
     // Start is called before the first frame update
     void Start()
     {
         // initialize variables
-        isStopped = true; // game begins with car stopped
-        isRight = true; // game begins with car on the right lane
+        game_Manager = FindObjectOfType<Game_Manager>();
+
+        isStopped = true; // game starts with car stopped
+        isRight = true; // game starts with car on the right lane
     }
 
     // Update is called once per frame
@@ -68,42 +70,48 @@ public class UI_Manager : MonoBehaviour
     // tell car go and stop, update buttons contextually
     public void StopGo_OnClick()
     {
-        if(isStopped)
+        if(!game_Manager.isGameOver)
         {
-            goButton.gameObject.SetActive(false);
-            stopButton.gameObject.SetActive(true);
-            isStopped = false;
+            if(isStopped)
+            {
+                goButton.gameObject.SetActive(false);
+                stopButton.gameObject.SetActive(true);
+                isStopped = false;
 
-            // add function call to Background Scroller (make car go)
-        }
-        else
-        {
-            goButton.gameObject.SetActive(true);
-            stopButton.gameObject.SetActive(false);
-            isStopped = true;
+                // add function call to Background Scroller (make car go)
+            }
+            else
+            {
+                goButton.gameObject.SetActive(true);
+                stopButton.gameObject.SetActive(false);
+                isStopped = true;
 
-            // add function call to Background Scroller (make car stop)
+                // add function call to Background Scroller (make car stop)
+            }
         }
     }
 
     // tell car switch to left and right lane, update buttons contextually
     public void ChangeLane_OnClick()
     {
-        if (isRight)
+        if (!game_Manager.isGameOver)
         {
-            leftButton.gameObject.SetActive(false);
-            rightButton.gameObject.SetActive(true);
-            isRight = false;
+            if (isRight)
+            {
+                leftButton.gameObject.SetActive(false);
+                rightButton.gameObject.SetActive(true);
+                isRight = false;
 
-            // add function call to Car Script (make car switch to left lane)
-        }
-        else
-        {
-            leftButton.gameObject.SetActive(true);
-            rightButton.gameObject.SetActive(false);
-            isRight = true;
+                // add function call to Car Script (make car switch to left lane)
+            }
+            else
+            {
+                leftButton.gameObject.SetActive(true);
+                rightButton.gameObject.SetActive(false);
+                isRight = true;
 
-            // add function call to Background Scroller (make car switch to right lane)
+                // add function call to Background Scroller (make car switch to right lane)
+            }
         }
     }
 
@@ -117,19 +125,19 @@ public class UI_Manager : MonoBehaviour
             heart2.gameObject.SetActive(false);
             heart1.gameObject.SetActive(false);
         }
-        if (currentLife == 1)
+        if(currentLife == 1)
         {
             heart3.gameObject.SetActive(false);
             heart2.gameObject.SetActive(false);
             heart1.gameObject.SetActive(true);
         }
-        if (currentLife == 2)
+        if(currentLife == 2)
         {
             heart3.gameObject.SetActive(false);
             heart2.gameObject.SetActive(true);
             heart1.gameObject.SetActive(true);
         }
-        if (currentLife >= 3)
+        if(currentLife >= 3)
         {
             heart3.gameObject.SetActive(true);
             heart2.gameObject.SetActive(true);
@@ -144,35 +152,32 @@ public class UI_Manager : MonoBehaviour
     }
 
     // show the gameOverPanel, show the highscore
-    public void ShowGameOverPanel(string currentHighscore)
+    public void ShowGameOverPanel(string currentBestScore)
     {
         questionPanel.gameObject.SetActive(false);
 
-        question.text = ($"{currentHighscore}");
+        question.text = ($"{currentBestScore}");
+
         gameOverPanel.gameObject.SetActive(true);
     }
 
-    // restart game (reloads entire scene)
+    // retry and start a new game
     public void Retry_OnClick()
     {
-        SceneManager.LoadScene(0);
+        game_Manager.StartNewGame();
     }
 
-    // quit the game, depending if in editor or live app, change method
+    // quit the game
     public void Quit_OnClick()
     {
-    #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-    #endif
-        Application.Quit();
+        game_Manager.QuitGame();
     }
 
     // show the questionPanel with currentQuestion, also sets a bool to know if answer is correct or wrong
-    public void ShowQuestionPanel(string currentQuestion, bool isItCorrect)
+    public void ShowQuestionPanel(string currentQuestion)
     {
-        isCorrect = isItCorrect;
-
         question.text = ($"{currentQuestion}");
+
         questionPanel.gameObject.SetActive(true);
     }
 
@@ -181,14 +186,7 @@ public class UI_Manager : MonoBehaviour
     {
         questionPanel.gameObject.SetActive(false);
 
-        if (isCorrect == true)
-        {
-            // add function call to Life Manager (add life)
-        }
-        else
-        {
-            // add function call to Life Manager (minus life)
-        }
+        game_Manager.YesAnswer();
     }
 
     // close the questionPanel, add life if answer was correct, minus life if answer was wrong
@@ -196,13 +194,6 @@ public class UI_Manager : MonoBehaviour
     {
         questionPanel.gameObject.SetActive(false);
 
-        if (isCorrect == false)
-        {
-            // add function call to Life Manager (add life)
-        }
-        else
-        {
-            // add function call to Life Manager (minus life)
-        }
+        game_Manager.NoAnswer();
     }
 }
