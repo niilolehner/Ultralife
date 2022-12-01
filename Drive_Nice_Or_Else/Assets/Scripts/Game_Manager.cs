22fe6843124752e069f0b5496bb80a5053ebfca0
Niilo Lehner <niilo.lehner@gmail.com>
2022-12-01 19:07:58 +0200

    Lay out Game_Manager

---
+++ Drive_Nice_Or_Else/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// WORK IN PROGRESS BY NIILO
+
+public class Game_Manager : MonoBehaviour
+{
+    // Declare variables/objects
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize variables/objects
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+
+    }
+
+    public void SetGameOver()
+    {
+
+    }
+
+    public void SetQuestionPhase()
+    {
+
+    }
+}
