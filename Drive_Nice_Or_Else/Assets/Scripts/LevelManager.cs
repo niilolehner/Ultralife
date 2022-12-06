9af07a1197933ab5a928ad9379c644e798588599
Li <92740157+Li-G-itescia@users.noreply.github.com>
2022-12-06 23:25:54 +0200

    Level Manager

---
+++ Drive_Nice_Or_Else/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+    Dictionary<int, LevelDesign> Levels = new Dictionary<int, LevelDesign>();
+    public int levelId;
+    
+
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelId = 0;
+        Initializelevels();
+    }
+
+    public LevelDesign GetLevelDesginlById(int index) {
+        return Levels[index];
+    }
+
+    public LevelDesign GetActualLevelDesign()
+    {
+        return Levels[levelId];
+    }
+
+    private List<Sprite> fillLevels(List<Sprite> allSpritesSigns, int numberOfSprite) 
+    {
+        List<Sprite> spriteForLevel = new List<Sprite>();
+        for (int i = 0; i < numberOfSprite; i++)
+        {
+            Sprite sprite = allSpritesSigns.OrderBy(e => Random.value).First();
+            spriteForLevel.Add(sprite);
+            allSpritesSigns.Remove(sprite);
+        }
+
+        return spriteForLevel;
+    }
+
+    private void Initializelevels()
+    {
+        List<Sprite> allSpritesSigns = Resources.LoadAll<Sprite>("Signs").ToList();
+
+        int stopCount = allSpritesSigns.Count % 4;
+        int levelId = 0;
+
+        while (allSpritesSigns.Count > stopCount)
+        {
+            List<Sprite> spriteForLevel = fillLevels(allSpritesSigns, 4);
+            Levels.Add(levelId, new LevelDesign(spriteForLevel, new List<Texture2D>()));
+            levelId++;
+        }
+
+        List<Sprite> spriteForLastLevel = fillLevels(allSpritesSigns, stopCount);
+        Levels.Add(levelId, new LevelDesign(spriteForLastLevel, new List<Texture2D>()));
+
+        Levels[0].GamePlaySprites.Add(Resources.Load<Texture2D>("GamePlayInstruction/BonusMalus"));
+        Levels[0].GamePlaySprites.Add(Resources.Load<Texture2D>("GamePlayInstruction/Question"));
+        Levels[1].GamePlaySprites.Add(Resources.Load<Texture2D>("GamePlayInstruction/Pedestrian"));
+    }
+}
+
+public class LevelDesign
+{
+    public List<Sprite> SignSprites;
+    public List<Texture2D> GamePlaySprites;
+
+    public LevelDesign(List<Sprite> SignSprites, List<Texture2D> GamePlaySprites)
+    {
+        this.SignSprites = SignSprites;
+        this.GamePlaySprites = GamePlaySprites;
+    }
+}
